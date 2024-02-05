4cd2a4b89b77aa974cf46b6ee4d2adef730161e4
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-02-05 16:13:57 -0500

    25 Many to Many

---
+++ api/Models/Portfolio.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace api.Models
+{
+    [Table("Portfolios")]
+    public class Portfolio
+    {
+        public string AppUserId { get; set; }
+        public int StockId { get; set; }
+        public AppUser AppUser { get; set; }
+        public Stock Stock { get; set; }
+    }
+}
