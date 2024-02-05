27b851b43dceb7337de561798ecbf11091cacdb7
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-02-05 10:01:01 -0500

    15 Comment Update [Put]

--- api/Mappers/CommentMappers.cs
+++ api/Mappers/CommentMappers.cs
@@ -30,5 +30,13 @@ namespace api.Mappers
                 StockId = stockId,
             };
         }
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto commentModel)
+        {
+            return new Comment
+            {
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+            };
+        }
     }
 }
