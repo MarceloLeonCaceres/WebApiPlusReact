aba6a80ad351cbf148b126a8bb894c06a4e41224
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-02-05 16:50:57 -0500

    26 Portfolio GET

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace api.Models
 {
@@ -12,9 +8,9 @@ namespace api.Models
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
-        [Column(TypeName ="decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase { get; set; }
-        [Column(TypeName ="decimal(18,2)")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
