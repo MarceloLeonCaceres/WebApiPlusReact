24c3f19aa331551fedec625941771753f10f6b84
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-02-06 09:30:26 -0500

    28 Portfolio DELETE

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -68,5 +68,25 @@ namespace api.Controllers
                 return Created();
             }
         }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepo.GetUserPortfolio(appUser);
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower());
+            if(filteredStock.Count() == 1)
+            {
+                await _portfolioRepo.DeletePortfolio(appUser, symbol);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Stock is not in your portfolio.");
+            }
+        }
     }
 }
